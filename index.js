9b96b7a3ff081cc9945793f7716b88bf920cdb48
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2022-03-27 20:13:20 +0400

    updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import 'dotenv/config'
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-schedule.scheduleJob('* 20 * * 0', function () { // отложенная рассылка сообщений
+schedule.scheduleJob('15 20 * * 0', function () { // отложенная рассылка сообщений
     bot.telegram.sendMessage(process.env.FAMILY_GROUP_ID, 'Hello, you need to go on english classes')
     bot.telegram.sendPoll(process.env.FAMILY_GROUP_ID, 'are you going?', ['yes', 'no'], { is_anonymous: false })
 })
