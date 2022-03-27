fecc3b3ca776705fd1893b3497e1f83eddb6af9d
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2022-03-27 20:29:08 +0400

    update timezone

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ import dayjs from 'dayjs';
 import schedule from "node-schedule";
 import 'dotenv/config'
 
+let rule = new schedule.RecurrenceRule();
+rule.tz = 'Europe/Samara';
+rule.second = 0;
+rule.minute = 30;
+rule.hour = 20;
+rule.dayOfWeek = 0
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-schedule.scheduleJob('15 20 * * 0', function () { // отложенная рассылка сообщений
+schedule.scheduleJob(rule, function () { // отложенная рассылка сообщений
     bot.telegram.sendMessage(process.env.FAMILY_GROUP_ID, 'Hello, you need to go on english classes')
     bot.telegram.sendPoll(process.env.FAMILY_GROUP_ID, 'are you going?', ['yes', 'no'], { is_anonymous: false })
 })
