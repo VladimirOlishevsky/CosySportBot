b1ab3fa5344c46d5586b8b45306223a212f540c4
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2022-03-27 19:32:18 +0400

    test delayed messaging

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ import 'dotenv/config'
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-schedule.scheduleJob('25 * * * *', function () { // отложенная рассылка сообщений
-    console.log('333')
-    bot.telegram.sendMessage(process.env.FAMILY_GROUP_ID, 'Hello everyone')
-    bot.telegram.sendPoll(process.env.FAMILY_GROUP_ID, 'test poll', ['1', '2'], { is_anonymous: false })
+schedule.scheduleJob('25 10 * * 1', function () { // отложенная рассылка сообщений
+    bot.telegram.sendMessage(process.env.FAMILY_GROUP_ID, 'Hello, you need to go on english classes')
+    bot.telegram.sendPoll(process.env.FAMILY_GROUP_ID, 'are you going?', ['yes', 'no'], { is_anonymous: false })
 })
 
 bot.start((ctx) => {
