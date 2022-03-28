5d4e93f6024502765728cb98b502aa413557f911
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2022-03-28 21:15:34 +0400

    testing messages on tuesday and wednesday

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ import 'dotenv/config'
 let rule = new schedule.RecurrenceRule();
 rule.tz = 'Europe/Samara';
 rule.second = 0;
-rule.minute = 25;
+rule.minute = 0;
 rule.hour = 10;
-rule.dayOfWeek = 1
+rule.dayOfWeek = [2, 3]
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const dayOfWeek = dayjs().format('dddd')
 
 schedule.scheduleJob(rule, function () { // отложенная рассылка сообщений
-    bot.telegram.sendMessage(process.env.FAMILY_GROUP_ID, 'Hello, you need to go on english classes')
-    bot.telegram.sendPoll(process.env.FAMILY_GROUP_ID, 'are you going?', ['yes', 'no'], { is_anonymous: false })
+    bot.telegram.sendMessage(process.env.FAMILY_GROUP_ID, `Hello, today is ${dayOfWeek}`)
+    bot.telegram.sendPoll(process.env.FAMILY_GROUP_ID, 'are you going on fotball?', ['yes', 'no'], { is_anonymous: false })
 })
 
 bot.start((ctx) => {
-    console.log('here');
+    console.log(ctx.update.message) // информация о id группового чата
 })
 
 bot.launch()
